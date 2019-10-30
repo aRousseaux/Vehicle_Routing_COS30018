3765ec35271db949832fd4639307fd03d44ccebd
aRousseaux <52120065+aRousseaux@users.noreply.github.com>
2019-10-30 23:44:01 +1100

    Added default case with file
    defaults/config has the default properties

--- src/gui/EditorForm.java
+++ src/gui/EditorForm.java
@@ -4,6 +4,9 @@ package gui;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.TextField;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -14,7 +17,7 @@ import data.DataModel;
 
 public class EditorForm extends JPanel
 {
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	private TextField fLocations;
 	private TextField fVehicles;
@@ -22,6 +25,7 @@ private static final long serialVersionUID = 1L;
 	private TextField fSeed;
 	private JComboBox<String> fRoutingMethods;
 	private JButton fSubmit;
+	private JButton fDefaults;
 	
 	public EditorForm( RoutingFrame aRoutingFrame )
 	{
@@ -109,6 +113,14 @@ private static final long serialVersionUID = 1L;
 		lConstraints.gridwidth = 2;
 		fSubmit.addActionListener( e -> onSubmit( aRoutingFrame ) );
 		this.add(fSubmit, lConstraints);
+		
+		fDefaults = new JButton("Default");
+		lConstraints.gridx = 0;
+		lConstraints.gridy = 6;
+		lConstraints.gridheight = 1;
+		lConstraints.gridwidth = 2;
+		fDefaults.addActionListener( e -> onDefault( aRoutingFrame ) );
+		this.add(fDefaults, lConstraints);
 	}
 
 	private void onSubmit( RoutingFrame aRoutingFrame ) 
@@ -123,6 +135,27 @@ private static final long serialVersionUID = 1L;
 				Integer.parseInt(fCapacity.getText())
 			);
 			aRoutingFrame.onSubmit( lDataModel, (String) fRoutingMethods.getSelectedItem() );
+		}
+		catch (Exception e) { e.printStackTrace(); }
+	}
+	
+	private void onDefault( RoutingFrame aRoutingFrame )
+	{
+		try
+		{
+			InputStream lDefaults = new FileInputStream("Defaults/config.properties");
+			
+			Properties lProperties = new Properties();
+			lProperties.load(lDefaults);
+			
+			DataModel lDataModel = new DataModel
+			(
+				Integer.parseInt(lProperties.getProperty("vehicles")), 
+				Integer.parseInt(lProperties.getProperty("locations")), 
+				Integer.parseInt(lProperties.getProperty("seed")), 
+				Integer.parseInt(lProperties.getProperty("capacity"))
+			);
+			aRoutingFrame.onSubmit( lDataModel, lProperties.getProperty("method") );
 		}
 		catch (Exception e) { e.printStackTrace(); }
 	}
